704fa656fc730e03f16f3c9c1ff61219897c689a
Brian Lee <brianleedongjun@gmail.com>
2017-12-21 18:33:09 -0800

    Update migration on Sites

--- db/migrate/20171222022533_create_sites.rb
+++ db/migrate/20171222022533_create_sites.rb
@@ -2,7 +2,10 @@ class CreateSites < ActiveRecord::Migration[5.1]
   def change
     create_table :sites do |t|
     	t.string :name, null: false
-    	t.string :address
+    	t.string :street_address
+    	t.string :city
+    	t.string :state
+    	t.string :country
    		t.decimal :latitude, null: false
    		t.decimal :longitude, null: false
 		t.timestamps
